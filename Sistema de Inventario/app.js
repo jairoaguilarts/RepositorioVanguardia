099d463fcff117097f1a0f629a79238ba61c317d
Jairo Aguilar <jairoaguilar1219@gmail.com>
2024-04-29 19:36:58 -0600

    Conectando a la base de datos

--- Sistema de Inventario/app.js
+++ Sistema de Inventario/app.js
@@ -4,6 +4,34 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Conexion a la base de datos en Atlas
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://DBAdmin:SLhO7Tunkw9fjx6W@inventario.ievreub.mongodb.net/?retryWrites=true&w=majority&appName=Inventario";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
